9e57988d92de666fbeeb5809e791248202425a10
JW Clark <this.clark@gmail.com>
2024-10-21 23:12:52 -0600

    Integrate openlayers map

--- src/main/scala/views/AltIndexView.scala
+++ src/main/scala/views/AltIndexView.scala
@@ -31,6 +31,7 @@ object AltIndexView {
         meta(charset := "UTF-8"),
         title("Weather"),
         link(rel := "stylesheet", href := "/static/css/bootstrap.min.css"),
+        link(rel := "stylesheet", href := "/static/css/openlayers.css"),
         link(rel := "stylesheet", href := "/static/css/main.css")
       ),
       body(
@@ -73,7 +74,28 @@ object AltIndexView {
             ),
             div(id := "tab-content3", cls := "tab-content")(
               h2("Map Search"),
-              p("Map search functionality goes here.")
+              div(id := "map", style := "width: 100%; height: 400px;"),
+              form(action := "/weather", method := "get", id := "mapForm", autocomplete := "off")(
+                div(cls := "mb-3")(
+                  label(`for` := "city", cls := "form-label")("City:"),
+                  input(`type` := "text", cls := "form-control", id := "city", name := "city", autocomplete := "off")
+                ),
+                div(cls := "mb-3")(
+                  label(`for` := "state", cls := "form-label")("State:"),
+                  input(`type` := "text", cls := "form-control", id := "state", name := "state", autocomplete := "off")
+                ),
+                div(cls := "mb-3")(
+                  label(`for` := "lat", cls := "form-label")("Latitude:"),
+                  input(`type` := "text", cls := "form-control", id := "lat", name := "latitude", autocomplete := "off")
+                ),
+                div(cls := "mb-3")(
+                  label(`for` := "long", cls := "form-label")("Longitude:"),
+                  input(`type` := "text", cls := "form-control", id := "long", name := "longitude", autocomplete := "off")
+                ),
+                div(cls := "mb-3")(
+                  input(`type` := "submit", cls := "btn btn-primary", value := "Submit")
+                )
+              )
             )
           ),
           div(cls := "row mt-4")(
@@ -106,7 +128,9 @@ object AltIndexView {
           )
         ),
         script(src := "/static/js/bootstrap.min.js"),
-        script(src := "/static/js/alt-autocomplete.js")
+        script(src := "/static/js/alt-autocomplete.js"),
+        script(src := "/static/js/openlayers.min.js"),
+        script(src := "/static/js/map.js")
       )
     ).render
   }
